26acdb466308a0efc3ef878865dcc88aaa438e3e
Juntao666 <jl13036@nyu.edu>
2025-11-07 15:32:56 -0500

    initialize navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { indigo, amber } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
 
+export const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: amber,
+  },
+});
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/page2" element={<HomePage />} />
+          <Route path="/page3" element={<HomePage />} /> */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
